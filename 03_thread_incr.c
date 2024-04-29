e8167c97ae9a28cdf852c2ddfb91624c55dee926
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-04-29 22:35:22 +0530

    Add files via upload

---
+++ 03_thread_incr.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+static int glob = 0;
+
+static void *thread_func(void *arg)
+{
+    int loops = *((int *)arg);
+    int loc, j;
+
+    for (j = 0; j < loops; j++)
+    {
+        loc = glob;
+        loc++;
+        glob = loc;
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t t1, t2;
+    int loops, s;
+
+    loops = (argc > 1) ? atoi(argv[1]) : 10000000;
+
+    s = pthread_create(&t1, NULL, thread_func, &loops);
+    if (s != 0)
+        perror("pthread_create()");
+
+    s = pthread_create(&t2, NULL, thread_func, &loops);
+    if (s != 0)
+        perror("pthread_create");
+
+    s = pthread_join(t1, NULL);
+    if (s != 0)
+        perror("pthread_join");
+
+    s = pthread_join(t2, NULL);
+    if (s != 0)
+        perror("pthread_join");
+
+    printf("glob = %d\n", glob);
+
+    exit(EXIT_SUCCESS);
+}
